a5a4a32e8cd7edd210d5df1e1e75d81fbf7d16c8
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-29 03:04:07 +0100

    Data_dowload.py functionality added

---
+++ nightmare_repo/Data Handling/data_download.py
@@ -0,0 +1,38 @@
+import yfinance as yf
+
+def download_data(ticker, start_date, end_date, period='1d'):
+    """
+    Downloads historical market data for a given ticker within a date range.
+
+    Args:
+    ticker (str): the ticker symbol for the security.
+    start_date (str): the start date for the data range in format 'YYYY-MM-DD'.
+    end_date (str): the end date for the data range in format 'YYYY-MM-DD'.
+    period (str): the frequency of data ('1d' for daily, '1wk' for weekly, '1mo' for monthly). Default is '1d'.
+
+    Returns:
+    pandas.DataFrame: the historical market data for the given ticker and date range.
+    """
+    # Download historical data as a pandas DataFrame
+    data = yf.download(ticker, start=start_date, end=end_date, interval=period)
+    
+    return data
+
+
+def save_data(data, filename):
+    """
+    Saves a pandas DataFrame to a CSV file.
+
+    Args:
+    data (pandas.DataFrame): the DataFrame to save.
+    filename (str): the name of the CSV file.
+    """
+    data.to_csv(filename)
+
+
+if __name__ == "__main__":
+    # Download daily data for the Russell 2000 Index
+    data = download_data('^RUT', '2010-01-01', '2023-06-28', period='1d')
+    
+    # Save the data to a CSV file
+    save_data(data, 'russell_2000_daily.csv')
